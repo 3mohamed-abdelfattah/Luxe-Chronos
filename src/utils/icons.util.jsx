3a0c5e8994abeae2cc547ca2b5957b1dff3ac64d
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-11-21 11:41:43 +0200

    main

---
+++ src/utils/icons.util.jsx
@@ -0,0 +1,5 @@
+export const SearchIcon = () => (
+    <svg width="41" height="40" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M38.7585 19.9495C38.7585 9.86478 30.5832 1.68945 20.4984 1.68945C10.4136 1.68945 2.23828 9.86478 2.23828 19.9495C2.23828 30.0344 10.4136 38.2096 20.4984 38.2096C30.5832 38.2096 38.7585 30.0344 38.7585 19.9495Z" stroke="white" stroke-width="3.04335" stroke-linejoin="round" />
+    </svg>
+);
