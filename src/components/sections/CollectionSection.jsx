cf0315fd9f14e2e6644989cbc08f21bd6826beb4
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-11-22 19:22:45 +0200

    Finish collection section

--- src/components/sections/CollectionSection.jsx
+++ src/components/sections/CollectionSection.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+// Component
+import { CollectionCard } from '@/components'
+// Assets
+import Watch1 from '@/assets/for_landing/collections/watch1.png'
+import Watch2 from '@/assets/for_landing/collections/watch2.png'
+import Watch3 from '@/assets/for_landing/collections/watch3.png'
 
 export const CollectionSection = () => {
     return (
-        <div>CollectionSection</div>
+        <main className="bg-secondaryColor flex flex-wrap justify-around items-center p-8">
+            <CollectionCard
+                watchImage={Watch1}
+                model="Rolex"
+                watchName="Sky Dweller"
+                price="26,800"
+            />
+            <CollectionCard
+                watchImage={Watch2}
+                model="Rolex"
+                watchName="Sea Dweller"
+                price="86,500"
+            />
+            <CollectionCard
+                watchImage={Watch3}
+                model="Rolex"
+                watchName="Air King"
+                price="94,300"
+            />
+        </main>
     )
 }
